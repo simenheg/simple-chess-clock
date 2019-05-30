514503d3c1fcdf9a689b0559d127700fa2b871f3
Simen Heggestøyl <simenheg@gmail.com>
2019-05-30 19:35:26 +0200

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((indent-tabs-mode . nil)
+         (tab-width . 4)
+         (fill-column . 79)))
+ (java-mode . ((c-file-offsets
+                (arglist-intro . +)
+                (arglist-cont . 0)
+                (arglist-close . 0)))))
